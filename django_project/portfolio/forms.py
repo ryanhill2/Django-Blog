e7ae34a9e66d87d19694255219b6ae751c470a16
ryan hill <c16443814@mydit.ie>
2020-02-04 19:26:56 +0000

    http POST request now working allowing users to add a stock

--- django_project/portfolio/forms.py
+++ django_project/portfolio/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from .models import stock
 
-class stockForm(forms.Form):
+class stockForm(forms.ModelForm):
     stock_ticker = forms.CharField(label='Stock Ticker')
     purchase_price = forms.CharField(label='Purchase Price')
     number_of_shares = forms.CharField(label='Number of Shares')
+
+    class Meta:
+        model = stock
+        fields = ('stock_ticker', 'purchase_price', 'number_of_shares',)
